14e60215231c9a8931bac1fcc420eafe38951c6e
xX0oo0Xx <siyuliang@uvic.ca>
2025-10-27 21:54:32 -0700

    添加Road结构体，尝试在主函数中实现道路

--- racing/main.cpp
+++ racing/main.cpp
@@ -1,7 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+
 using namespace sf;
 const int WINDOW_WIDTH = 1024;
 const int WINDOW_HEIGHT = 768;
+const int roadWidth = 1800;
+const int roadsegmentLength = 180;
+const int roadcount = 2000;
+
+struct Road {
+	float x, y, z;
+	float X, Y, Z;
+
+	Road(int _x, int _y, int _z) : x(_x), y(_y), z(_z) {}
+		void project(int camX, int camY, int camZ) {
+			float scale = 1.0f / (z - camZ);
+			X = (1 + scale * (x - camX)) * WINDOW_WIDTH / 2;
+			Y = (1 - scale * (y - camY)) * WINDOW_HEIGHT / 2;
+			Z = scale * roadWidth * WINDOW_WIDTH / 2;
+		
+	}
+};
 
 void DrawTrape(RenderWindow& window, Color c, int x1, int y1,
 	int w1, int x2, int y2, int w2) {
@@ -17,6 +36,14 @@ void DrawTrape(RenderWindow& window, Color c, int x1, int y1,
 int main() {
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Racing Game");
 	window.setFramerateLimit(60);
+
+	std::vector<Road> roads;
+	for (int i = 0; i < roadcount; ++i) {
+		Road r(0, 0, (i+1) * roadsegmentLength);
+		roads.push_back(r);
+	}
+
+
 	while (window.isOpen()) {
 		Event event;
 		while (window.pollEvent(event)) {
@@ -25,8 +52,22 @@ int main() {
 		}
 		window.clear(Color::Black);
 		// Game rendering and logic would go here
-		DrawTrape(window, Color::White, WINDOW_WIDTH / 2, 500, 200, WINDOW_WIDTH / 2
-			, 300, 100);
+		/*DrawTrape(window, Color::White, WINDOW_WIDTH / 2, 500, 200, WINDOW_WIDTH / 2
+			, 300, 100);*/
+
+		for (int i = 0; i < roadcount; ++i) {
+			Road& now = roads[i];
+			now.project(0, 1500, 0);
+
+			if (!i) {
+				continue;
+			}
+			Road& prev = roads[i-1];
+			Color road = i % 2 ? Color(105, 105, 105) : Color(101, 101, 101);
+			DrawTrape(window, road,
+				prev.X, prev.Y, prev.Z,
+				now.X, now.Y, now.Z);
+		}
 		window.display();
 	}
 
